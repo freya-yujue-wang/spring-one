79f11f9460b547ec219015464d0e7c46d8875d6c
wyjsusan <yujuewa@gmail.com>
2019-08-01 11:26:41 -0700

    complete the TrackCoach and GymCoach.

--- spring-one/src/com/wyjsusan/springdemo/GymCoach.java
+++ spring-one/src/com/wyjsusan/springdemo/GymCoach.java
@@ -1,13 +1,19 @@
 package com.wyjsusan.springdemo;
 
 public class GymCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	public GymCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
 	public String getDailyWorkout() {
 		return "Spend 60 minutes on lifting weights.";
 	}
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Wow" + fortuneService.getFortune();
 	}
 }
